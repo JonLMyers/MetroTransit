9bebaf5289a17459715b36b6209b7b638eeffc2d
Blekkspruter <jlm4508@rit.edu>
2018-03-07 03:51:00 -0500

    Created vital functions for modularity

--- MetroTransit/login.php
+++ MetroTransit/login.php
@@ -3,6 +3,7 @@
 require_once 'db.php';
  
 // Define variables and initialize with empty values
+$mysqli = $con;
 $username = $password = "";
 $username_err = $password_err = "";
 $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
@@ -26,53 +27,62 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($con, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $user_username, $password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if($user_password == $password){
-                            /* Password is correct, so start a new session and
-                            save the username to the session */
-                            session_start();
-                            $_SESSION['username'] = $username;
-                            $_SESSION['id'] = $id;
-                            $_SESSION['isauth'] = 'yes';
-                            setcookie("admin", "false", time() + (86400 * 30), "/");    
-                            header("location: /");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = 'The password you entered was not valid.';
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = 'No account found with that username.';
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+        if(Authenticate_High($username, $user_password, $mysqli)){
+            header("location: /");
         }
-        
-        // Close statement
-        mysqli_stmt_close($stmt);
+        else{
+            $username_err = $password_err = "Invalid Credentials";
+        }
     }
 }
+
+function Authenticate_High($param_username, $user_password, $mysqli){
+    // Prepare a select statement
+    $sql = "SELECT id, username, password FROM users WHERE username = ?";
+
+    if($stmt = mysqli_prepare($mysqli, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            // Store result
+            mysqli_stmt_store_result($stmt);
+            
+            // Check if username exists, if yes then verify password
+            if(mysqli_stmt_num_rows($stmt) == 1){                    
+                // Bind result variables
+                mysqli_stmt_bind_result($stmt, $id, $user_username, $password);
+                if(mysqli_stmt_fetch($stmt)){
+                    if($user_password == $password){
+                        /* Password is correct, so start a new session and
+                        save the username to the session */
+                        session_start();
+                        $_SESSION['username'] = $param_username;
+                        $_SESSION['id'] = $id;
+                        $_SESSION['isauth'] = 'yes';
+                        setcookie("admin", "false", time() + (86400 * 30), "/");    
+                    } else{
+                        // Display an error message if password is not valid
+                        $password_err = 'The password you entered was not valid.';
+                        return False;
+                    }
+                }
+            } else{
+                // Display an error message if username doesn't exist
+                $username_err = 'No account found with that username.';
+                return False;
+            }
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+
+    // Close statement
+    mysqli_stmt_close($stmt);
+    return True;
+}
+
 ?>
  
 <!DOCTYPE html>
--- MetroTransit/register.php
+++ MetroTransit/register.php
@@ -1,6 +1,7 @@
 <?php
     // include database connection
     include 'db.php';
+    $mysqli = $con;
 
     $username = $password = $confirm_password = "";
     $username_err = $password_err = $confirm_password_err = "";
@@ -12,34 +13,11 @@
         if(empty(trim($_POST["username"]))){
             $username_err = "Please enter a username.";
         } else{
-            // Prepare a select statement
-            $sql = "SELECT id FROM users WHERE username = ?";
-            $mysqli = $con;
-
-            if($stmt = $mysqli->prepare($sql)){
-                // Bind variables to the prepared statement as parameters
-                $stmt->bind_param("s", $param_username);
-                
-                // Set parameters
-                $param_username = trim($_POST["username"]);
-                
-                // Attempt to execute the prepared statement
-                if($stmt->execute()){
-                    // store result
-                    $stmt->store_result();
-                    
-                    if($stmt->num_rows == 1){
-                        $username_err = "This username is already taken.";
-                    } else{
-                        $username = trim($_POST["username"]);
-                    }
-                } else{
-                    echo "Oops! Something went wrong. Please try again later.";
-                }
+            $param_username = trim($_POST["username"]);
+            $username = ValidateUsername_High($param_username, $mysqli);
+            if($username == "This username is already taken."){
+                $username_err = $username;
             }
-            
-            // Close statement
-            $stmt->close();
         }
         
         // Validate password
@@ -50,7 +28,7 @@
         } else{
             $password = trim($_POST['password']);
         }
-        
+    
         // Validate confirm password
         if(empty(trim($_POST["confirm_password"]))){
             $confirm_password_err = 'Please confirm password.';     
@@ -63,35 +41,69 @@
         
         // Check input errors before inserting in database
         if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-            
-            // Prepare an insert statement
-            $sql = "INSERT INTO users (username, password, credits) VALUES (?, ?, '300.00')";
-            
-            if($stmt = $mysqli->prepare($sql)){
-                // Bind variables to the prepared statement as parameters
-                $stmt->bind_param("ss", $param_username, $param_password);
-                
-                // Set parameters
-                $param_username = $username;
-                $param_password = $password;
-                
-                // Attempt to execute the prepared statement
-                if($stmt->execute()){
-                    // Redirect to login page
-                    header("location: login.php");
-                } else{
-                    echo "Something went wrong. Please try again later.";
-                }
-            }
-            
-            // Close statement
-            $stmt->close();
+            Register_High($username, $password, $mysqli);
         }
         
+    }
+
+    function ValidateUsername_High($param_username, $mysqli){
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
+
+        if($stmt = $mysqli->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("s", $param_username);
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // store result
+                $stmt->store_result();
+                
+                if($stmt->num_rows == 1){
+                    $username_err = "This username is already taken.";
+                    $stmt->close();
+                    return $username_err;
+                } else{
+                    $username = $param_username;
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        $stmt->close();
+
+        return $username;
+        
+    }
+
+    function Register_High($param_username, $param_password, $mysqli){
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, password, credits) VALUES (?, ?, '300.00')";
+
+        if($stmt = $mysqli->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("ss", $param_username, $param_password);
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+        else{
+            echo "Something went wrong. Please try again later.";
+        }
+
         // Close connection
         $mysqli->close();
     }
-    ?>
+
+
+?>
 
 
 <!DOCTYPE html>
